9254d26e95bb393f43651a51fdfa8e40d9a76f52
00Flash <anonimowyflash@wp.planonimowyflash@wp.pl>
2023-03-27 16:38:39 +0200

    Initial commit

---
+++ src/test/java/com/dominikpalichleb/trainingapp/TrainingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dominikpalichleb.trainingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
