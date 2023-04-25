3d5039105cd5416c3363edb764a9dee2d22238d6
00Flash <anonimowyflash@wp.planonimowyflash@wp.pl>
2023-04-25 12:03:09 +0200

    User and registration services

---
+++ src/main/java/com/dominikpalichleb/trainingapp/service/UserService.java
@@ -0,0 +1,26 @@
+package com.dominikpalichleb.trainingapp.service;
+
+import com.dominikpalichleb.trainingapp.domain.model.User;
+import com.dominikpalichleb.trainingapp.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(String.format("Username %s not found", username)));
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+}
